2638819a3cb31cfc946d5323a3245219e6f4be5c
washington.dasilva <washington.dasilva@dataprev.gov.br>
2021-09-19 01:34:45 -0300

    encode

--- src/main/java/br/com/project/impl/IncrementadorImpl.java
+++ src/main/java/br/com/project/impl/IncrementadorImpl.java
@@ -7,7 +7,7 @@ public class IncrementadorImpl implements Incrementador{
 	@Override
 	public int incrementa(int valor) {
 		
-		System.out.println("Acessando a implementação da interface.");
+		System.out.println("Acessando a implementaÃ§Ã£o da interface.");
 		valor++;
 		return valor;
 		
